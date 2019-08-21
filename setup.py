4a8e94a29e2823bd9f81761a6ac7f018b1228e55
GuardianGH <zhling2012@live.com>
2019-08-21 15:18:36 +0800

    update files

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="imrepltool",
-    version="0.0.2",
+    version="0.1.4",
     author="GuardianAngel",
     author_email="zhling2012@live.com",
-    description="check the image contains the specified image template and set to cover it up with color or your logo",
+    description="check the image contains the specified image template and set to cover it up",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/GuardianGH/sxclzy",
+    url="https://github.com/GuardianGH/imrepltool/tree/master/imrepltool",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -19,8 +19,15 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'psutil>=5.6.2',
-        'prettytable>=0.7.2',
-        'SQLAlchemy>=1.3.3',
+        "decorator>=4.4.0",
+        "imageio>=2.5.0",
+        "matplotlib>=3.1.1",
+        "numpy>=1.17.0",
+        "Pillow>=6.1.0",
+        "PyMySQL>=0.9.3",
+        "SQLAlchemy>=1.3.7",
+        "requests>=2.22.0",
+        "tqdm>=4.32.1",
+        "scikit-image>=0.15.0",
     ]
 )
